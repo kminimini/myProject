20b5c684ccc059e9aab48f8965338b97b974423b
kminimini <gitgtgtg@naver.com>
2024-05-20 21:53:03 +0900

    open api test

--- api-module/src/main/java/com/weather/api/ApiApplication.java
+++ api-module/src/main/java/com/weather/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.weather.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.weather.common", "com.weather.service", "com.weather.api.uploadController"})
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
--- service-module/src/main/java/com/weather/service/service/WeatherService.java
+++ service-module/src/main/java/com/weather/service/service/WeatherService.java
@@ -1,4 +1,7 @@
 package com.weather.service.service;
 
 public interface WeatherService {
+    boolean saveWeatherData(String nx, String ny, String baseDate, String baseTime);
+
+    String getWeatherForecast(int nx, int ny, String baseDate, String baseTime);
 }
